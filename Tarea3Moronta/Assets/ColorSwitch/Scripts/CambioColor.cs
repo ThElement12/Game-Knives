8fba362142541b1d23229c256ebf5231a8464ab6
Jean Carlos <jean18699@gmail.com>
2019-03-15 22:05:42 -0400

    Bajen eta vaina primero

---
+++ Tarea3Moronta/Assets/ColorSwitch/Scripts/CambioColor.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CambioColor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Material colorObjeto = other.transform.GetComponent<Renderer>().material;
+        int colorAleatorio = Random.Range(1, 4);
+        switch(colorAleatorio)
+        {
+            case 1:
+                colorObjeto.color = Color.red;
+                break;
+
+            case 2:
+                colorObjeto.color = Color.blue;
+                break;
+
+            case 3:
+                colorObjeto.color = Color.yellow;
+                break;
+
+            case 4:
+                colorObjeto.color = Color.green;
+                break;
+
+        }
+    }
+
+
+
+}
---
+++ Tarea3Moronta/Assets/ColorSwitch/Scripts/ControlBola.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlBola : MonoBehaviour
+{
+
+    //Asignando a la bola una velocidad constante de bajada
+    Vector3 Velocidad;// = new Vector3 (0f,-10f);
+
+    Vector3 Movimiento;
+    float tiempoSalto;
+    bool salto = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Movimiento.y = Velocidad.y * Time.deltaTime + (Physics.gravity.y * Mathf.Pow(Time.deltaTime, 2) / 2);
+        if (salto == true)
+        {
+            Velocidad.y = 50f;
+            salto = false;
+        }
+        else 
+        {
+            Velocidad.y = -5f;
+        }
+        transform.Translate(Movimiento);
+        
+        if(Input.GetButtonDown("Fire1"))
+        {
+
+            Saltar();
+        }
+        
+        //   Velocidad = Physics.gravity * Time.deltaTime;
+    }
+
+    void Saltar()
+    {
+        salto = true;
+        //transform.GetComponent<Rigidbody>().AddForce(new Vector3(0, 1f, 0),ForceMode.Impulse);
+    }
+
+}
