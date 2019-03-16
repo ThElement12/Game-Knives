263c4fc2c0eb54f8091929e2aee0a5d8d15c7509
ThElemenet12 <45013990+ThElemenet12@users.noreply.github.com>
2019-03-16 10:45:39 -0400

    Formulas fisicas aplicadas

--- Tarea3Moronta/Assets/ColorSwitch/Scripts/ControlBola.cs
+++ Tarea3Moronta/Assets/ColorSwitch/Scripts/ControlBola.cs
@@ -6,44 +6,29 @@ public class ControlBola : MonoBehaviour
 {
 
     //Asignando a la bola una velocidad constante de bajada
-    Vector3 Velocidad = Vector3.zero;// = new Vector3 (0f,-10f);
+    Vector3 Velocidad = Vector3.zero;
+    Vector3 posicion = Vector3.zero;
 
-    Vector3 Movimiento;
     //float tiempoSalto;
-    bool salto = false;
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+        posicion.x = Velocidad.x * Time.deltaTime + Physics.gravity.x * (Mathf.Pow(Time.deltaTime, 2) / 2);
+        posicion.y = Velocidad.y * Time.deltaTime + Physics.gravity.y * (Mathf.Pow(Time.deltaTime, 2) / 2);
 
-        Movimiento.y = Velocidad.y * Time.deltaTime + (Physics.gravity.y * Mathf.Pow(Time.deltaTime, 2) / 2);
-        if (salto == true)
+        gameObject.transform.Translate(posicion);
+
+        Velocidad += Physics.gravity * Time.deltaTime;
+
+        if (Input.GetButtonDown("Fire1"))
         {
-            Velocidad.y = 50f;
-            salto = false;
+            posicion = Vector3.zero;
+            Velocidad = new Vector3(0,7);
         }
-        else 
-        {
-            Velocidad.y = -5f;
-        }
-        transform.Translate(Movimiento);
-        
-        if(Input.GetButtonDown("Fire1"))
-        {
-            Saltar();
-        }
-        
-        //   Velocidad = Physics.gravity * Time.deltaTime;
     }
-
-    void Saltar()
-    {
-        salto = true;
-        //transform.GetComponent<Rigidbody>().AddForce(new Vector3(0, 1f, 0),ForceMode.Impulse);
-    }
-
 }
---
+++ Tarea3Moronta/Assets/ColorSwitch/Scripts/MCUObstaculo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MCUObstaculo : MonoBehaviour
+{
+    Vector3 rotacion = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotacion = new Vector3(0, 0, Random.Range(1, 3));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(rotacion);
+    }
+}
