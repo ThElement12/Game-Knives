8b7247650c1b039f356e6eecae16d3f3f8455015
jean18699 <jean18699@gmail.com>
2019-03-16 22:48:18 -0400

    Colision por tipo de color

---
+++ Tarea3Moronta/Assets/ColorSwitch/Scripts/DeteccionColor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeteccionColor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Material colorObjeto = other.gameObject.GetComponent<Renderer>().material;
+        Material colorCuerpo = transform.gameObject.GetComponent<Renderer>().material;
+
+        if(colorObjeto.color == colorCuerpo.color)
+        {
+            return;
+        }else
+        {
+            Destroy(other.gameObject);
+        }
+
+    }
+
+}
