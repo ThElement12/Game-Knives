cafa35dd65f2ec234179346e955641ff02c90fe1
Ruben <rubenosmani.dc@gmail.com>
2019-03-18 20:13:24 -0400

    Aqui estamos
    asdfa

---
+++ Tarea3Moronta/Assets/KnifeHit/Scripts/HitSomething.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitSomething : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (gameObject.tag == "Target")
+        {
+            if (other.gameObject.tag == "Knife")
+            {
+                other.gameObject.GetComponent<Shoot>().isShooting = false;
+                other.gameObject.transform.parent = gameObject.transform;
+            }
+        }
+
+        else if (gameObject.tag == "Knife")
+        {
+            if(other.gameObject.tag == "Knife")
+            {
+                Destroy(GameObject.FindGameObjectWithTag("Target"));
+            }
+        }
+    }
+}
---
+++ Tarea3Moronta/Assets/KnifeHit/Scripts/Shoot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public bool isShooting = false;
+    Vector3 Velocidad = Vector3.zero;
+    Vector3 position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!isShooting) 
+        {
+            Velocidad.y = 3f;
+            position.y += Velocidad.y * Time.deltaTime;
+            gameObject.transform.Translate(position);
+        }
+
+    }
+}
