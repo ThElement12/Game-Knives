0e670148c2b1cd3dd4ecee8454f3d44b4f1c9a1b
Ruben <rubenosmani.dc@gmail.com>
2019-03-23 19:25:53 -0400

    Menu

---
+++ Tarea3Moronta/Assets/Scripts/MenuControl.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour
+{
+    const float SCALEFACTOR = 1.2f;
+    AudioManager _audioManager;
+    private void Awake()
+    {
+      _audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
+    }
+    private void OnMouseEnter()
+    {
+        _audioManager.PlayHoverSound();
+        gameObject.transform.localScale *= SCALEFACTOR;
+    }
+
+    private void OnMouseExit()
+    {
+        //_audioManager.PlayExitSound();
+        gameObject.transform.localScale /= SCALEFACTOR;
+    }
+
+    private void OnMouseUp()
+    {
+        _audioManager.PlayClickedSound();
+        switch (gameObject.name)
+        {
+            case "Play":
+                SceneManager.LoadScene("KnifeHit");
+                break;
+            case "Options":
+                break;
+            case "Quit":
+                Application.Quit(0);
+                break;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
