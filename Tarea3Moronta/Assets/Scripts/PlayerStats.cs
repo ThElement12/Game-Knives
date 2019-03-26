ea06d0d73d8989f7a17f7a2f9b7243c13a4e717f
Ruben <rubenosmani.dc@gmail.com>
2019-03-26 16:12:23 -0400

    HighScore
    Almost Working properly

---
+++ Tarea3Moronta/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts
+{
+    [DataContract]
+    public class PlayerStats
+    {
+        [DataMember]
+        public string PlayerName { get; set; }
+        [DataMember]
+        public int Points { get; set; }
+    }
+}
---
+++ Tarea3Moronta/Assets/Scripts/SaveStats.cs
@@ -0,0 +1,54 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using UnityEngine;
+
+public class SaveStats : MonoBehaviour
+{
+    string rutaXML;
+    public static PlayerStats CurrentGame;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentGame = new PlayerStats();
+        rutaXML = Application.persistentDataPath + "/HighScores.xml";
+        CurrentGame.PlayerName = ControlKniveHit.playername;
+        CurrentGame.Points = ControlKniveHit.points;
+        Debug.Log(rutaXML);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ControlKniveHit.kniveState == ControlKniveHit.Estate.End)
+        {
+            SaveState();
+        }
+    }
+
+    public void SaveState()
+    {
+        DataContractSerializer dcSerializer = new DataContractSerializer(typeof(PlayerStats));
+
+        using (FileStream fstream = new FileStream(rutaXML, FileMode.Create))
+        {
+            dcSerializer.WriteObject(fstream, CurrentGame);
+        }
+
+
+    }
+
+    public void LoadState()
+    {
+        DataContractSerializer dcSerializer = new DataContractSerializer(typeof(PlayerStats));
+
+        using (FileStream fstream = new FileStream(rutaXML, FileMode.Open))
+        {
+            CurrentGame = (PlayerStats)dcSerializer.ReadObject(fstream);
+        }
+
+
+    }
+}
