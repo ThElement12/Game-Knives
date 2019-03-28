187fdd1678dee099d81bc6315422c15f8423fbaf
Ruben <rubenosmani.dc@gmail.com>
2019-03-28 16:31:38 -0400

    new powerUp

--- Tarea3Moronta/Assets/Scripts/ControlKniveHit.cs
+++ Tarea3Moronta/Assets/Scripts/ControlKniveHit.cs
@@ -23,7 +23,7 @@ public class ControlKniveHit : MonoBehaviour
     public static bool LastHit;
     public static int points;
     public static Estate kniveState;
-    public static int knives;
+    public static int knives, powercount;
     GameObject actualTarget;
     GameObject knivepunto;
     // Start is called before the first frame update
@@ -49,6 +49,10 @@ public class ControlKniveHit : MonoBehaviour
                     knife = GameObject.FindGameObjectWithTag("Knife");
                     if(knife != null)
                      knife.GetComponent<Shoot>().isShooting = true;
+                    if(powercount == 0)
+                    {
+                        HitSomething.pointPLus = 1;
+                    }
                 }
 
                 if (knives == 0 && LastHit)
@@ -108,6 +112,10 @@ public class ControlKniveHit : MonoBehaviour
     public static void GastarAmmo()
     {
         Destroy(KnivesPuntos[knives - 1].gameObject);
+        if(HitSomething.pointPLus > 0)
+        {
+            powercount--;
+        }
         knives--;
     }
 }
